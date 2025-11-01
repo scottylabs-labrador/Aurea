e7605cb8cd260079cdc218a33d99933219e93b4c
Ryan Wu <ryanwusd@gmail.com>
2025-11-01 17:38:10 -0400

    added/tested tone.js

--- src/react/App.tsx
+++ src/react/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import CameraFeed from './CameraFeed';
+import * as Tone from "tone";
 
 export default function App() {
+    const start = async () => {
+        await Tone.start();
+        console.log("started")
+        //create a synth and connect it to the main output (your speakers)
+        const synth = new Tone.Synth().toDestination();
+
+        //play a middle 'C' for the duration of an 8th note
+        synth.triggerAttackRelease("C4", "8n");
+    }
   return (
     <div>
       <h1>Aurea </h1>
+      <button onClick={start}>Start</button>
       <CameraFeed />
     </div>
   );
